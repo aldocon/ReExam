dc4e621ebb5164b1eeea2eb6a366e1ca939e8ac7
aldocon <aldocon38@gmail.com>
2016-07-27 04:09:06 +0200

    added and configured NHibernate...and it was a PAIN in the ass

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.Areas.Admin.ViewModels;
 using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
+using NHibernate.Linq;
+
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
@@ -14,7 +18,15 @@ namespace SimpleBlog.Areas.Admin.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new UsersIndex
+
+            {
+                Users = Database.Session.Query<User>().ToList()
+                //Session.Query<User>().ToList
+            }
+                
+            
+                );
         }
 
 
