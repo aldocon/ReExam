b9280ecede93dd6a3aa83c0a362d4ad9ca315089
aldocon <aldocon38@gmail.com>
2016-07-29 19:03:21 +0200

    added anti-forgery-tokens to all but the  delete user.

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -37,7 +37,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
             });
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult New(UsersNew form)
         {
             if(Database.Session.Query<User>().Any(u => u.Username == form.Username))
@@ -72,7 +72,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
 
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Edit(int id, UsersEdit form)
         {
             var user = Database.Session.Load<User>(id);
@@ -106,7 +106,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
             
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult ResetPassword(int id, UsersResetPassword form)
         {
             var user = Database.Session.Load<User>(id);
--- SimpleBlog/Areas/Admin/Scripts/Forms.js
+++ SimpleBlog/Areas/Admin/Scripts/Forms.js
@@ -7,11 +7,17 @@
 
                 if (message && !confirm(message))
                     return;
-                
-                $("<form>")
+
+                //var antiForgeryToken = $("#anti-forgery-form input");
+                //var antiForgeryInput = $("<input type='hidden'").attr("name", antiForgeryToken.attr("name")).val(antiForgeryToken.val());
+
+            $("<form>")
                     .attr("method", "post")
                     .attr("action", $this.attr("href"))
+                    //.append($("<input type='hidden' name=''>").val($("#anti-forgery-form").find("input").val()))
                     .appendTo(document.body)
                     .submit();
             });
-    });
+});
+
+// den grønne kode som er udkommenteret var til at genere en anti-forgery token til delete formen.                    
