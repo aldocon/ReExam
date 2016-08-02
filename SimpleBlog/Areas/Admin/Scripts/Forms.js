f93d88d9471d5106f9f79b0969bed1be9fd2268a
aldocon <aldocon38@gmail.com>
2016-08-02 19:04:56 +0200

    delete store and thrash posts and slugify component, warning slugify is not functional and neither is hard delete, the dropdown button is not working and antiforgery token validation and generation is missing.

--- SimpleBlog/Areas/Admin/Scripts/Forms.js
+++ SimpleBlog/Areas/Admin/Scripts/Forms.js
@@ -17,7 +17,35 @@
                     //.append($("<input type='hidden' name=''>").val($("#anti-forgery-form").find("input").val()))
                     .appendTo(document.body)
                     .submit();
-            });
+        });
+
+        //$("[data-slug]").each(function () {
+        //    var $this = $(this);
+        //    var $sendSlugFrom = $($this.data("slug"));
+
+        //    $sendSlugFrom.keyup(function() {
+        //        var slug = $sendSlugFrom.val();
+        //        slug = slug.replace(/[^a-zA-Z0-9\s]/g, "");
+        //        slug = slug.toLocaleLowerCase();
+        //        slug = slug.replace(/\s+/g, "-");
+
+        //        if (slug.charAt(slug.length - 1) == "-")
+        //            slug = slug.substr(0, slug.length - 1);
+
+        //        $this.val(slug);
+
+    //    });/
+    //});
+
+    //, data_slug = "'#Title" this line was taken from the form.cshtml line 25, the slug field.
+
+    // above code was removed it was giving problems and is in the end only a nice to have not a need to have.
+
+
+        
+
 });
+
+
 
 // den grønne kode som er udkommenteret var til at genere en anti-forgery token til delete formen.                    
